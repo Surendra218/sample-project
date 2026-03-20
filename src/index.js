55ac0212c10038feaab0ae5db12a464acb63432f
Raghav <ks490256@gmail.com>
2026-03-20 01:50:24 -0400

    Add runtime test to catch require errors + re-introduce requrie typo bug

--- src/index.js
+++ src/index.js
@@ -9,6 +9,21 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get('/users', async (req, res) => {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+  const users = _.map(response.data, user => ({
+    id: user.id,
+    name: user.name,
+    email: user.email
+  }));
+  res.json(users);
 });
+
+// Only start server when run directly, not when required in tests
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
